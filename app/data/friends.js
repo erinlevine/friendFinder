d35cc7f44f8ded4ffbc3aee178320241641f91b4
erinlevine <erinclevine@gmail.com>
2017-06-29 17:39:32 -0400

    Added sloth photos to Friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,29 @@
 var friendsArray = [
 {
-	friendName: "Ruby",
-	friendPicture: "http://i.imgur.com/OimO1tF.jpg",
-	surveyResults: [ '1', '2', '3', '2', '3', '1', '5', '3', '1', '3' ]
+	friendName: "Walter",
+	friendPicture: "http://i.imgur.com/ajFH31J.jpg?2",
+	surveyResults: [ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' ]
 },
 {
 
-	friendName: "Pete",
-	friendPicture: "http://www.google.com",
-	surveyResults: [ '4', '1', '5', '4', '3', '1', '2', '1', '4', '2']
+	friendName: "Buttercup",
+	friendPicture: "http://i.imgur.com/FWvuFoL.jpg?2",
+	surveyResults: [ '5', '1', '5', '4', '3', '2', '2', '1', '4', '1']
+},
+{
+	friendName: "Pineapple",
+	friendPicture: "http://i.imgur.com/k0qDF8w.jpg?1",
+	surveyResults: [ '5', '5', '5', '5', '5', '5', '5', '5', '5', '5' ]
+},
+{
+	friendName: "Bob",
+	friendPicture: "http://i.imgur.com/yGNuAgx.jpg",
+	surveyResults: [ '3', '4', '2', '1', '5', '3', '4', '5', '2', '2' ]
+},
+{
+	friendName: "Eugene",
+	friendPicture: "http://i.imgur.com/r6jYYau.jpg?1",
+	surveyResults: [ '5', '3', '4', '5', '2', '4', '3', '2', '1', '4' ]
 }
 ];
 
--- server.js
+++ server.js
@@ -1,45 +1,28 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
+// Dependencies:
 
 var express = require("express");
 var bodyParser = require("body-parser");
 
-
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
-
-// Tells node that we are creating an "express" server
+//Setting up express:
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+// Initial port:
 var PORT = process.env.PORT || 8080;
 
-// BodyParser makes it possible for our server to interpret data sent to it.
-// The code below is pretty standard.
+// BodyParser:
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
 
+// Routing:
 require("./app/routing/apiRoutes")(app);
 
 require('./app/routing/htmlRoutes')(app);
 
-// ==============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// ==============================================================================
 
+// Listener:
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
