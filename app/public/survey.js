a8313e645cecc42b87dfd92c9120b30528ba0db4
erinlevine <erinclevine@gmail.com>
2017-06-29 15:39:23 -0400

    Almost there

---
+++ app/public/survey.js
@@ -0,0 +1,27 @@
+$("#submit").on("click", function(){
+ 
+
+var userData = {
+	    		friendName: $("#name").val(),
+	    		friendPicture: $("#photo").val(),
+	    		surveyResults: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val(), $("#q6").val(), $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val(), ]
+	    	}
+
+
+	    	// Grab the URL of the website
+	    	var currentURL = window.location.origin;
+
+	    	// AJAX post the data to the friends API. 
+	    	$.post(currentURL + "/api/friends", userData, function(data){
+	    		console.log(data);
+	    		//Grab the result from the AJAX post so that the best match's name and photo are displayed.
+	    		$("#matchName").text(data.friendName);
+	    		$('#matchImg').attr("src", data.friendPicture);
+
+		    	// Show the modal with the best match 
+		    	$("#resultsModal").modal('toggle');
+
+	    	});
+
+
+    });
